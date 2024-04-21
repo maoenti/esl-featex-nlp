d23de51e0a698ab7adc9c08270ffc7c8cf3918f6
maoenti <melvyndede@gmail.com>
2024-04-21 15:27:31 +0700

    WIP: featex v2, main verb

---
+++ src/pipeline/extract_features_v2.py
@@ -0,0 +1,20 @@
+from src.models.feature_extraction_v2 import FeatureExtractionV2
+from src.utils.file_handling import set_training_data
+
+def extract_features_v2(data, filename, nlp):
+    extracted_features = []
+    for item in data:
+        response = {}
+
+        response['question_text'] = item['question_text']
+        featex_v2 = FeatureExtractionV2(item, nlp)
+        opt_deps = featex_v2.option_deps()
+        response['v1'] = opt_deps['v1']
+        response['v2'] = opt_deps['v2']
+        response['v3'] = opt_deps['v3']
+        extracted_features.append(response)
+        if response['v2']:
+            print(response)
+    # set_training_data(f'{filename}_features_v2', extracted_features)
+
+    return extracted_features
